a8e79378612ad39cb4d7b3c747c450e81651a414
CheshtaDang <cheshtadang@gmail.com>
2020-07-17 16:59:59 +0530

    Bird trajectroy

--- Bird.js
+++ Bird.js
@@ -2,12 +2,22 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.trajectory = [];
   }
 
   display() {
+
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
 
     super.display();
+    if(this.body.velocity.x >10 && this.body.position.x >200){
+    var position = [this.body.position.x,this.body.position.y];
+    this.trajectory.push(position);
+    }
+    for(var counter = 0;counter<this.trajectory.length;counter++){
+      image(this.smokeImage,this.trajectory[counter][0],this.trajectory[counter][1]);
+    }
   }
 }
